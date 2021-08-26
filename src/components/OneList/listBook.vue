e57854822c0fd0a959d38115c491ce01ba3833ed
Airgo1 <nicolas.burdet@sfr.fr>
2021-08-26 17:45:18 +0200

    Revert "fix folders components"
    This reverts commit c87bc9bfb7f462b49d8b952db35efea62b053b2c.

---
+++ src/components/OneList/listBook.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div  class="title">
+        <h2>{{ this.ListBestSellers.display_name }}</h2>
+    </div>
+
+    <b-container fluid="lg" class="container-card">
+        <b-row v-if="!loadingList" cols="2">
+            <cardBook v-for="book in ListBestSellers.books" v-bind:key="book.title" v-bind:book="book" />
+        </b-row>
+        <div class="spining" v-else>
+          <b-spinner label="Spinning"></b-spinner>      
+        </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import cardBook from '@/components/oneList/CardBook'
+import axios from 'axios'
+axios.defaults.baseURL = 'https://api.nytimes.com/svc/books/v3/'
+export default {
+  name: 'listBook',
+  components: {
+    cardBook
+  },
+  props: {
+    date: String,
+    nameTheme: String
+  },
+  data () {
+    return {
+      loadingList: true,
+      ListBestSellers: [],
+      nbrResult: null
+    }
+  },
+  created () {
+    this.getBestSellersList()
+  },
+  methods: {
+    getBestSellersList () {
+      this.loadingList = true
+      var config = {
+        method: 'get',
+        url: axios.defaults.baseURL + '/lists/' + this.date + '/' + this.nameTheme + '.json?api-key=kg5NbaANHHWdCoQNd7lMd6UGbRkrvoyi',
+        headers: { }
+      }
+
+      axios(config)
+        .then(response => {
+          this.ListBestSellers = response.data.results
+          this.nbrResult = response.data.num_results
+          this.loadingList = false
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+.container-card {
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+.spining{
+  text-align: center;
+}
+</style>
---
+++ src/components/oneList/ModalBook.vue
@@ -0,0 +1,126 @@
+<template>
+    <div>
+      <b-modal v-model="isModalVisible" size="xl" hide-footer>
+        <template #modal-title>
+            <strong> {{ book.title }} </strong> <br>
+            by {{ book.author }}
+        </template>
+
+        <b-container >
+            <b-row>
+                <b-col >
+                    <b-img :src="book.book_image" fluid alt="Responsive image"></b-img>
+                </b-col>
+
+                <b-col>
+                    <strong> ISBN : </strong> {{book.primary_isbn13}} <br>
+                    <strong> Publisher : </strong> {{book.publisher}} <br>
+                    <strong> Contributor : </strong> {{book.contributor}} <br>
+                    <br>
+                    <strong> description : </strong> {{book.description}} <br>
+                    <br>
+                    <strong> Price : </strong> {{book.price}} €
+                </b-col>
+            </b-row>
+        </b-container>
+        <b-container>
+            <div class="center">
+                <b-button @click="addBookToCard">Add to Card</b-button>
+            </div>
+        </b-container>
+
+        </b-modal>
+
+        <b-modal :id="'add-validation-model-'+book.title" size="xl" hide-header hide-footer>
+            <diV>
+                {{ book.title }} is add to card
+            </diV>
+            <div>
+                <b-button href="#/cart">Go to cart</b-button>
+                <b-button @click="continueBuy">Continue to buy</b-button>
+            </div>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'modalBook',
+  props: {
+    bookProps: Object,
+    isModalVisibleProps: Boolean
+  },
+  data () {
+    return {
+      book: this.bookProps,
+      isModalVisible: this.isModalVisibleProps,
+      cart: [],
+      bookCart: {
+        title: null,
+        isbn: null,
+        image: null,
+        price: null,
+        quantity: 1
+      }
+    }
+  },
+  created () {
+    this.book.price = this.book.primary_isbn13 % 30 + 10
+
+    this.bookCart.title = this.book.title
+    this.bookCart.isbn = this.book.primary_isbn13
+    this.bookCart.image = this.book.book_image
+    this.bookCart.price = this.book.price
+  },
+  methods: {
+    addBookToCard () {
+      var added = false
+      this.cart.forEach(element => {
+        if (element.title === this.bookCart.title) {
+          element.quantity += 1
+          added = true
+        }
+      })
+      if (added !== true) {
+        this.cart.push(this.bookCart)
+      }
+
+      this.saveCart()
+      this.$bvModal.show('add-validation-model-'+this.book.title)
+    },
+    getLocalStorageCard () {
+      if (localStorage.getItem('cart')) {
+        this.cart = JSON.parse(localStorage.getItem('cart'))
+      }
+    },
+    saveCart () {
+      const parsed = JSON.stringify(this.cart)
+      localStorage.setItem('cart', parsed)
+    },
+    continueBuy () {
+      this.$bvModal.hide('add-validation-model-'+this.book.title)
+    }
+  },
+  watch: {
+    isModalVisibleProps (newVal) {
+      this.isModalVisible = newVal
+      if (newVal === true) {
+        this.getLocalStorageCard('add-validation-model-'+this.book.title)
+      }
+    },
+    isModalVisible (newVal) {
+      this.$emit('update:isModalVisible', newVal)
+    }
+  }
+}
+</script>
+
+<style>
+.center {
+  margin: 0;
+  position: absolute;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
